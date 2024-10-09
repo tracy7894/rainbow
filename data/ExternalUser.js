09c6c334a421b0cb02a872694dd7993a8ad27496
tracy7894 <tracy20071015@gmail.com>
2024-10-09 08:23:30 +0800

    1009

--- data/ExternalUser.js
+++ data/ExternalUser.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 let ExternalUser=new mongoose.Schema({
     username: { type: String, required: true },
     password:{ type: String, required: true },
-    group:{ type: String, required: true },
+    group:{ type: String },
     name:{ type: String, required: true },
     age:{ type: String, required: true },
     gender:{ type: String, required: true },
