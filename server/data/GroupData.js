dd5aa3f82e3cea7f36d99f9ad60a625130eb2ade
tracy7894 <tracy20071015@gmail.com>
2025-05-29 21:40:18 +0800

    完善教材相關api＋調整資料庫

--- server/data/GroupData.js
+++ server/data/GroupData.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 let GroupData = new mongoose.Schema({
-    ID: { type: Number, require: true },
+    ID: { type: Number, required: true },
     name: { type: String, required: true },//組別名
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'StudentUser' }], // 擁有學生 外部不分組
     createdAt: { type: Date, default: Date.now }
