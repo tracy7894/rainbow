6c79338d150dc713b1e24ac936d42ddecfc39a98
tracy7894 <tracy20071015@gmail.com>
2025-06-19 21:09:53 +0800

    資料集合更新

--- server/data/StudentQuizScore.js
+++ server/data/StudentQuizScore.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 //學生測驗成績紀錄
 const StudentQuizScoreSchema = new mongoose.Schema({
-    student: { type: mongoose.Schema.Types.ObjectId, ref: "StudentUser", required: true }, // 學生 
-
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'studentModel' // 動態參照 studentModel
+    },
+    studentModel: { type: String, required: true, enum: ['StudentUser', 'ExternalUser'] },
     quizId: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz', required: true },
     answers: [// 選擇的答案 自動對應原始題目
         {
-          questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizData.questions' }, // 題目 ID
-          selectedOption: Number 
+            questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizData.questions' }, // 題目 ID
+            selectedOption: Number
         }
-      ], 
-    score: { type: Number ,default:null},
+    ],
+    score: { type: Number, default: null },
     submittedAt: { type: Date, default: Date.now }
 });
 
